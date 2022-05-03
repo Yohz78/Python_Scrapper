50654a9ebae296a2e3ba05c25037b9b990bf29b1
LeoGx <lgarrigoux@gmail.com>
2022-05-03 14:34:07 +0200

    iteration avec dictionnaire

--- DraftScrapper.py
+++ DraftScrapper.py
@@ -3,15 +3,32 @@ from bs4 import BeautifulSoup
 
 url = 'http://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html'
 
-response = requests.get(url)
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+articles = soup.find_all('article',class_="product_pod")
 
-if response.ok:
-	links = []
-	soup = BeautifulSoup(response.text, 'html.parser')
-	lis = soup.findAll('li',class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
-	for li in lis:
-		a = li.findAll('a')
-		link = soup.a['href']
-		links.append(link)
+# # on récupère le titre
+# titres = [art.find('img').get('alt') for art in articles]
 
-print(soup)
+# # on récupère les prix
+# prices = [art.find(class_='price_color').get_text() for art in articles]	
+
+# # on récupère l'URL
+# links = [art.find("a").get('href') for art in articles]
+
+# # on récupère la disponibilité
+# dispos = [art.find(class_="instock availability").get_text().replace("\n","").strip() for art in articles]
+
+# On enregistre direct les éléments dans un dico
+formatted_articles = []
+
+for art in articles:
+	data = {
+		'title': art.find('a').get('title'),
+		"url": art.find("a").get('href'),
+		"price": art.find(class_='price_color').get_text(),
+		"dispo": art.find(class_="instock availability").get_text().replace("\n","").strip()
+	}
+	formatted_articles.append(data)
+
+print(formatted_articles)
