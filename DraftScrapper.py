cc4869960b9215413fc16a3b20b836f8bed4bd8f
LeoGx <lgarrigoux@gmail.com>
2022-05-05 21:48:33 +0200

    capable de scrapper tout le site, manque l'extraction en CSV

--- DraftScrapper.py
+++ DraftScrapper.py
@@ -1,20 +1,6 @@
 import explorer
-import category
-
-# Execute le code : Extrait toutes les données produits de toutes les pages d'une catégorie à partir de sa page d'acceuil
-def page_scrapper(url):	
-	url_tool = url.replace('index.html','')
-	urls = explorer.explorer(url)
-	datas = urls.data_geter()
-	next_page = urls.get_next_page()
-	while next_page:	
-		page = explorer.explorer(url_tool + next_page)
-		datas.append(page.data_geter())
-		next_page = page.get_next_page()			
-	return datas
+import pandas as pd 
 
 # Testeur :
-# test = page_scrapper('https://books.toscrape.com/catalogue/category/books/historical-fiction_4/index.html')
-test = category.category('https://books.toscrape.com/index.html')
-test_print = test.url_getter()
-print(test_print)
+test = explorer.explorer('https://books.toscrape.com/index.html')
+print(test.site_getter())
--- explorer.py
+++ explorer.py
@@ -17,11 +17,11 @@ class explorer:
         return soup
 
     def get_next_page(self):
-        """Vérifie si la page suivant existe et la renvoie le cas échéant"""
+        """Vérifie si la page suivante existe et la renvoie le cas échéant"""
         next_page = self.soup.find('li', class_="next")
         if next_page:
             page = self.soup.find('li', class_="next").select('a')[
-                    0].get('href')
+                0].get('href')
             return page
         else:
             return None
@@ -30,6 +30,31 @@ class explorer:
         """Récupère toutes les infos des produits d'une page"""
         articles = self.soup.find_all('article', class_='product_pod')
         urls = [art.find("a").get('href').replace(
-        "../../..", "http://books.toscrape.com/catalogue") for art in articles]
+            "../../..", "http://books.toscrape.com/catalogue") for art in articles]
         datas = [scrapper.Scrapper(url).table_analysis() for url in urls]
         return datas
+
+    def page_scrapper(url):
+        """Renoie une liste de toutes les données des produits d'une catégorie"""
+        url_tool = url.replace('index.html', '')
+        urls = explorer(url)
+        datas = urls.data_geter()
+        next_page = urls.get_next_page()
+        while next_page:
+            page = explorer(url_tool + next_page)
+            datas.append(page.data_geter())
+            next_page = page.get_next_page()
+        return datas
+
+    def url_getter(self):
+        """renvoie une liste de toutes les URLs des catégories du site"""
+        ul = self.soup.find('ul',class_="nav nav-list").select('li')[0]
+        lis = ul.find_all('li') 
+        urls = [("https://books.toscrape.com/") + li.find("a").get('href') for li in lis]
+        return urls    
+
+    def site_getter(self):
+        """renvoie toutes les datas de tous les articles du site : YOLO """
+        categories = self.url_getter()
+        datas = [explorer.page_scrapper(category) for category in categories]
+        return datas 
